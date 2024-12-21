a60620714f96acf338cdd4a3a5a6f9e805035504
Rgisop2 <160823869+Rgisop2@users.noreply.github.com>
2024-12-22 03:55:24 +0530

    Update env.ts

--- env.ts
+++ env.ts
@@ -4,7 +4,7 @@ import { cleanEnv, str } from "envalid";
 await config({ export: true });
 
 export default cleanEnv(Deno.env.toObject(), {
-  BOT_TOKEN: str(),
-  OWNERS: str(),
-  MONGO_URL: str(),
+  BOT_TOKEN: str(8140111290:AAHkbPYHFIEiOIrQlGaNRDFyuUv-qvu2Mi8),
+  OWNERS: str(1327021082),
+  MONGO_URL: str(mongodb+srv://rgruhan0:Ruhan9366@cluster0.wum1lek.mongodb.net/?retryWrites=true&w=majority),
 });
